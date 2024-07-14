16ff26f530f5dae33fb34b664fbec000bf32134b
gang_dramma <your_email@kottaholakottahola@gmail.com>
2024-07-14 16:54:56 +0500

    fix bugs

--- src/pages/auth/register/components/register.tsx
+++ src/pages/auth/register/components/register.tsx
@@ -6,49 +6,85 @@ import * as yup from "yup";
 import { Input } from "../../../../components";
 
 const schema = yup.object().shape({
-  username: yup.string().required().min(6).max(25),
-  password: yup.string().required().min(6).max(30),
-  gmail: yup.string().required().email(),
+  username: yup
+    .string()
+    .required("Необходимо указать имя пользователя")
+    .min(6, "Имя пользователя должно быть не менее 6 символов")
+    .max(25, "Имя пользователя должно содержать не более 25 символов"),
+  password: yup
+    .string()
+    .required("Необходимо ввести пароль")
+    .min(6, "Пароль должен быть не менее 6 символов")
+    .max(30, "Пароль должен содержать не более 30 символов"),
+  gmail: yup
+    .string()
+    .required("Необходимо указать gmail")
+    .email("Неверный формат gmail"),
 });
 
 interface RegisterState {
   values: IForm.IRegister;
-  errors: Partial<IForm.IRegister>;
+  errors: Partial<Record<keyof IForm.IRegister, string>>;
 }
 
 interface RegisterProps {}
 
-export default class Login extends Component<RegisterProps, RegisterState> {
+export default class Register extends Component<RegisterProps, RegisterState> {
   state: RegisterState = {
     values: { username: "", password: "", gmail: "" },
     errors: {},
   };
 
-  handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
-    let { values } = this.state;
+  validate = async (values: IForm.IRegister) => {
+    const errors: Partial<Record<keyof IForm.IRegister, string>> = {};
+
     try {
-      await schema.validate(values, {
-        abortEarly: false,
-      });
-      this.setState({ errors: {} });
+      await schema.validate(values, { abortEarly: false });
+      return null;
     } catch (error: any) {
-      const errors: Record<string, string> = {};
       if (error instanceof yup.ValidationError) {
         for (const { path = "", message } of error.inner) {
           errors[path as keyof IForm.IRegister] = message;
         }
       }
-      this.setState({ errors });
+      return errors;
     }
   };
 
-  handleChange: React.ChangeEventHandler<HTMLInputElement> = ({ target }) => {
-    this.setState((prev) => ({
-      ...prev,
-      values: { ...prev.values, [target.name]: target.value },
-    }));
+  handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    const errors = await this.validate(this.state.values);
+
+    if (errors) {
+      this.setState({ errors });
+      return;
+    }
+
+    console.log("values = ", this.state.values);
   };
+
+  handleChange: React.ChangeEventHandler<HTMLInputElement> = async ({
+    target,
+  }) => {
+    const { name, value } = target;
+    this.setState(
+      (prevState) => ({
+        values: {
+          ...prevState.values,
+          [name]: value,
+        },
+      }),
+      async () => {
+        const errors = await this.validate(this.state.values);
+        if (errors) {
+          this.setState({ errors });
+        } else {
+          this.setState({ errors: { ...this.state.errors, [name]: "" } });
+        }
+      }
+    );
+  };
+
   renderInput = (
     name: keyof IForm.IRegister,
     type: React.HTMLInputTypeAttribute = "text",
@@ -65,34 +101,33 @@ export default class Login extends Component<RegisterProps, RegisterState> {
       />
     );
   };
+
   render() {
     return (
-      <>
-        <main>
-          <div className="auth-container">
-            <div className="auth-header">
-              <div className="auth-text">Создайте аккаунт</div>
-              <div className="auth-second-text">
-                Создайте учетную запись, чтобы получить больше возможностей
-              </div>
-            </div>
-            <div className="auth-body">
-              <form onSubmit={this.handleSubmit}>
-                {this.renderInput("username", "text", "username")}
-                {this.renderInput("password", "password", "password")}
-                {this.renderInput("gmail", "email", "email")}
-                <button className="auth-submit-btn">РЕГИСТРАЦИЯ</button>
-                <div className="auth-go-to">
-                  У вас уже есть аккаунт?{" "}
-                  <span>
-                    <Link to={"/auth/login"}>ВОЙТИ</Link>
-                  </span>
-                </div>
-              </form>
+      <main>
+        <div className="auth-container">
+          <div className="auth-header">
+            <div className="auth-text">Создайте аккаунт</div>
+            <div className="auth-second-text">
+              Создайте учетную запись, чтобы получить больше возможностей
             </div>
           </div>
-        </main>
-      </>
+          <div className="auth-body">
+            <form onSubmit={this.handleSubmit}>
+              {this.renderInput("username", "text", "Имя пользователя")}
+              {this.renderInput("password", "password", "Пароль")}
+              {this.renderInput("gmail", "email", "Gmail")}
+              <button className="auth-submit-btn">РЕГИСТРАЦИЯ</button>
+              <div className="auth-go-to">
+                У вас уже есть аккаунт?{" "}
+                <span>
+                  <Link to={"/auth/login"}>ВОЙТИ</Link>
+                </span>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
     );
   }
 }
